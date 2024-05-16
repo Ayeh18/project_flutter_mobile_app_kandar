2fc99c629d76da3d633e66a9d3973079f0a942b2
Muniif <muniif10@gmail.com>
2024-05-17 00:08:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# project_flutter_mobile_app_kandar
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/project_flutter_mobile_app_kandar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_flutter_mobile_app_kandar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
